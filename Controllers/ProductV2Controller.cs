75097987ce04e36a00bcd6129ac13636ce962505
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-03-04 13:07:44 +0330

    :)

--- Controllers/ProductV2Controller.cs
+++ Controllers/ProductV2Controller.cs
@@ -12,23 +12,22 @@ public class ProductV2Controller : BaseApiController {
 	public async Task<ActionResult<GenericResponse<ProductEntity>>> Create(ProductCreateUpdateDto dto, CancellationToken ct)
 		=> Result(await _repository.Create(dto, ct));
 
-    [HttpPost("CreateWithMedia")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public async Task<ActionResult<GenericResponse<ProductEntity>>> CreateWithMedia([FromForm] ProductCreateUpdateDto dto, CancellationToken ct)
-        => Result(await _repository.CreateWithFiles(dto, ct));
+	[HttpPost("CreateWithMedia")]
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public async Task<ActionResult<GenericResponse<ProductEntity>>> CreateWithMedia([FromForm] ProductCreateUpdateDto dto, CancellationToken ct)
+		=> Result(await _repository.CreateWithFiles(dto, ct));
 
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 	[AllowAnonymous]
 	[HttpPost("Filter")]
-	public ActionResult<GenericResponse<IQueryable<ProductEntity>>> Filter(ProductFilterDto dto) => Result(_repository.Filter(dto));
+	public async Task<ActionResult<GenericResponse<IQueryable<ProductEntity>>>> Filter(ProductFilterDto dto) => Result(await _repository.Filter(dto));
 
 	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 	[AllowAnonymous]
 	[HttpGet("{id:guid}")]
-	public async Task<ActionResult<GenericResponse<ProductEntity>>> ReadById(Guid id, CancellationToken ct)
-		=> Result(await _repository.ReadById(id, ct));
+	public async Task<ActionResult<GenericResponse<ProductEntity>>> ReadById(Guid id, CancellationToken ct) => Result(await _repository.ReadById(id, ct));
 
-    [HttpPut]
+	[HttpPut]
 	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 	public async Task<ActionResult<GenericResponse<ProductEntity>>> Update(ProductCreateUpdateDto dto, CancellationToken ct)
 		=> Result(await _repository.Update(dto, ct));
