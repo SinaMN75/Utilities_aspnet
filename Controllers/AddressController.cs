0b11e020b04b136f9ba5a081d846a99b07860b93
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-05-12 11:50:38 +0330

    code clean

--- Controllers/AddressController.cs
+++ Controllers/AddressController.cs
@@ -1,36 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Utilities_aspnet.Controllers;
 
-namespace Utilities_aspnet.Controllers
-{
-    [ApiController]
-    [Route("api/address")]
-    public class AddressController : BaseApiController
-    {
-        private readonly IAddressRepository _repository;
-        public AddressController(IAddressRepository repository) => _repository = repository;
+[ApiController]
+[Route("api/address")]
+public class AddressController : BaseApiController {
+	private readonly IAddressRepository _repository;
+	public AddressController(IAddressRepository repository) => _repository = repository;
 
-        [HttpPost]
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [OutputCache(PolicyName = "10s")]
-        public async Task<ActionResult<GenericResponse<AddressEntity>>> Create(AddressCreateUpdateDto UserAddressDto) => Result(await _repository.Create(UserAddressDto));
+	[HttpPost]
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public async Task<ActionResult<GenericResponse<AddressEntity>>> Create(AddressCreateUpdateDto UserAddressDto)
+		=> Result(await _repository.Create(UserAddressDto));
 
-        [HttpPut]
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [OutputCache(PolicyName = "10s")]
-        public async Task<ActionResult<GenericResponse<AddressEntity>>> Update(AddressCreateUpdateDto UserAddressDto) => Result(await _repository.Update(UserAddressDto));
+	[HttpPut]
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public async Task<ActionResult<GenericResponse<AddressEntity>>> Update(AddressCreateUpdateDto UserAddressDto)
+		=> Result(await _repository.Update(UserAddressDto));
 
-        [HttpGet("ReadMyAddresses")]
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [OutputCache(PolicyName = "10s")]
-        public async Task<ActionResult<GenericResponse<IEnumerable<AddressEntity>>>> GetUserAddresses() => Result(await _repository.GetMyAddresses());
+	[HttpGet("ReadMyAddresses")]
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	[OutputCache(PolicyName = "10s")]
+	public async Task<ActionResult<GenericResponse<IEnumerable<AddressEntity>>>> ReadUserAddresses() => Result(await _repository.GetMyAddresses());
 
-        [HttpPut("Delete")]
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [OutputCache(PolicyName = "10s")]
-        public async Task<ActionResult<GenericResponse>> Delete(Guid addressId) => Result(await _repository.DeleteAddress(addressId));
-    }
+	[HttpPut("Delete")]
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public async Task<ActionResult<GenericResponse>> Delete(Guid addressId) => Result(await _repository.DeleteAddress(addressId));
 }
