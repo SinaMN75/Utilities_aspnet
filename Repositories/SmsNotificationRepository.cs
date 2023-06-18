8c9386bc1fe7c38ca5ddf6a0eec7be643c6fbb21
Mohamadhosein <mhroohiamini@gmail.com>
2023-06-18 08:00:37 +0330

    test 1

--- Repositories/SmsNotificationRepository.cs
+++ Repositories/SmsNotificationRepository.cs
@@ -1,117 +1,148 @@
-﻿namespace Utilities_aspnet.Repositories;
+﻿using Newtonsoft.Json.Linq;
 
-public interface ISmsNotificationRepository {
-	void SendSms(string mobileNumber, string message);
-	public Task<GenericResponse> SendNotification(NotificationCreateDto dto);
+namespace Utilities_aspnet.Repositories;
+
+public interface ISmsNotificationRepository
+{
+    void SendSms(string mobileNumber, string message);
+    public Task<GenericResponse> SendNotification(NotificationCreateDto dto);
 }
 
-public class SmsNotificationRepository : ISmsNotificationRepository {
-	private readonly IConfiguration _config;
+public class SmsNotificationRepository : ISmsNotificationRepository
+{
+    private readonly IConfiguration _config;
 
-	public SmsNotificationRepository(IConfiguration config) => _config = config;
+    public SmsNotificationRepository(IConfiguration config) => _config = config;
 
-	public void SendSms(string mobileNumber, string message) {
-		AppSettings appSettings = new();
-		_config.GetSection("AppSettings").Bind(appSettings);
-		SmsPanelSettings smsSetting = appSettings.SmsPanelSettings;
+    public async void SendSms(string mobileNumber, string message)
+    {
+        AppSettings appSettings = new();
+        _config.GetSection("AppSettings").Bind(appSettings);
+        SmsPanelSettings smsSetting = appSettings.SmsPanelSettings;
 
-		if (mobileNumber.Contains("+98")) {
-			mobileNumber = mobileNumber.TrimStart(new[] { '+' });
-			mobileNumber = mobileNumber.TrimStart(new[] { '9' });
-			mobileNumber = mobileNumber.TrimStart(new[] { '8' });
-		}
-		else { mobileNumber = mobileNumber.TrimStart(new[] { '0' }); }
+        if (mobileNumber.Contains("+98"))
+        {
+            mobileNumber = mobileNumber.TrimStart(new[] { '+' });
+            mobileNumber = mobileNumber.TrimStart(new[] { '9' });
+            mobileNumber = mobileNumber.TrimStart(new[] { '8' });
+        }
+        else { mobileNumber = mobileNumber.TrimStart(new[] { '0' }); }
 
-		switch (smsSetting.Provider) {
-			case "ghasedak": {
-				Api sms = new(smsSetting.SmsApiKey);
-				sms.VerifyAsync(1, smsSetting.PatternCode, new[] { mobileNumber }, message);
-				break;
-			}
-			case "faraz": {
-				var body = new {
-					op = "pattern",
-					user = smsSetting.UserName,
-					pass = smsSetting.SmsSecret,
-					fromNum = "03000505".TrimStart(new[] { '0' }),
-					toNum = mobileNumber,
-					patternCode = smsSetting.PatternCode,
-					inputData = "[{\"verification-code\":" + message + "}]}"
-				};
+        switch (smsSetting.Provider)
+        {
+            case "ghasedak":
+                {
+                    Api sms = new(smsSetting.SmsApiKey);
+                    sms.VerifyAsync(1, smsSetting.PatternCode, new[] { mobileNumber }, message);
+                    break;
+                }
+            case "faraz":
+                {
+                    //var body = new
+                    //{
+                    //    op = "pattern",
+                    //    user = smsSetting.UserName,
+                    //    pass = smsSetting.SmsSecret,
+                    //    fromNum = "03000505".TrimStart(new[] { '0' }),
+                    //    toNum = mobileNumber,
+                    //    patternCode = smsSetting.PatternCode,
+                    //    inputData = "[{\"verification-code\":" + message + "}]}"
+                    //};
 
-				RestClient client = new("http://ippanel.com/api/select");
-				RestRequest request = new(Method.POST);
-				request.AddHeader("cache-control", "no-cache");
-				request.AddHeader("Content-Type", "application/json");
-				request.AddHeader("Authorization", "AccessKey " + smsSetting.SmsApiKey);
-				request.AddParameter("undefined",
-				                     "{\"op\" : \"pattern\"" + ",\"user\" : \"" + smsSetting.UserName + "\"" + ",\"pass\": \"" + smsSetting.SmsSecret + "\"" +
-				                     ",\"fromNum\" : " + "03000505".TrimStart(new[] { '0' }) + "" + ",\"toNum\": " + mobileNumber + "" +
-				                     ",\"patternCode\": \" " +
-				                     smsSetting.PatternCode + "\"" + ",\"inputData\" : [{\"verification-code\":" + message + "}]}", ParameterType.RequestBody);
+                    //RestClient client = new("http://ippanel.com/api/select");
+                    //RestRequest request = new(Method.POST);
+                    //request.AddHeader("cache-control", "no-cache");
+                    //request.AddHeader("Content-Type", "application/json");
+                    //request.AddHeader("Authorization", "AccessKey " + smsSetting.SmsApiKey);
+                    //request.AddParameter("undefined",
+                    //                     "{\"op\" : \"pattern\"" + ",\"user\" : \"" + smsSetting.UserName + "\"" + ",\"pass\": \"" + smsSetting.SmsSecret + "\"" +
+                    //                     ",\"fromNum\" : " + "03000505".TrimStart(new[] { '0' }) + "" + ",\"toNum\": " + mobileNumber + "" +
+                    //                     ",\"patternCode\": \" " +
+                    //                     smsSetting.PatternCode + "\"" + ",\"inputData\" : [{\"verification-code\":" + message + "}]}", ParameterType.RequestBody);
 
-				client.Execute(request);
-				break;
-			}
-		}
-	}
+                    //var d = client.Execute(request);
 
-	public async Task<GenericResponse> SendNotification(NotificationCreateDto dto) {
-		AppSettings appSettings = new();
-		_config.GetSection("AppSettings").Bind(appSettings);
-		PushNotificationSetting setting = appSettings.PushNotificationSetting;
 
-		switch (setting.Provider) {
-			case "pushe": {
-				var body = new {
-					app_ids = setting.AppId,
-					data = new {
-						title = dto.Title,
-						content = dto.Content,
-						bigContent = dto.BigContent,
-						action = new { action_type = dto.ActionType, url = dto.Url }
-					},
-					is_draft = false,
-					filter = new { custom_id = dto.UserIds }
-				};
+                    var body = "{\"op\" : \"pattern\"" + ",\"user\" : \"" + smsSetting.UserName + "\"" + ",\"pass\": \"" + smsSetting.SmsSecret + "\"" +
+                                         ",\"fromNum\" : " + "03000505".TrimStart(new[] { '0' }) + "" + ",\"toNum\": " + mobileNumber + "" +
+                                         ",\"patternCode\": \" " +
+                                         smsSetting.PatternCode + "\"" + ",\"inputData\" : [{\"verification-code\":" + message + "}]}";
 
-				CustomHttpClient<object, object> client = new();
-				client.DefaultRequestHeaders.Add("Content-Type", "application/json");
-				client.DefaultRequestHeaders.Add("Authorization", "Token " + setting.Token);
-				client.DefaultRequestHeaders.Add("Content-Type", "application/json");
-				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "Token " + setting.Token);
+                    CustomHttpClient<object, string> client = new();
+                    
+                    using var customMessage = new HttpRequestMessage();
+                    var headers = customMessage.Headers;
+                    headers.Add("cache-control", "no-cache");
+                    headers.Add("Authorization", "AccessKey " + smsSetting.SmsApiKey);
 
-				object result = await client.Post("https://api.pushe.co/v2/messaging/notifications/", body);
-				break;
+                    var result = await client.Post("http://ippanel.com/api/select",body, headers);
+                    break;
+                }
+        }
+    }
 
-				//RestRequest request = new(Method.POST);
-				//request.AddHeader("Content-Type", "application/json");
-				//request.AddHeader("Authorization", "Token " + setting.Token);
-				//var body = new {
-				//	app_ids = setting.AppId,
-				//	data = new {
-				//		title = dto.Title,
-				//		content = dto.Content,
-				//		bigContent = dto.BigContent,
-				//		action = new {action_type = dto.ActionType, url = dto.Url}
-				//	},
-				//	is_draft = false,
-				//	filter = new {custom_id = dto.UserIds}
-				//};
-				//request.AddJsonBody(body);
+    public async Task<GenericResponse> SendNotification(NotificationCreateDto dto)
+    {
+        AppSettings appSettings = new();
+        _config.GetSection("AppSettings").Bind(appSettings);
+        PushNotificationSetting setting = appSettings.PushNotificationSetting;
 
-				//IRestResponse i = await new RestClient("https://api.pushe.co/v2/messaging/notifications/").ExecuteAsync(request);
-			}
-		}
-		return new GenericResponse();
-	}
+        switch (setting.Provider)
+        {
+            case "pushe":
+                {
+                    var body = new
+                    {
+                        app_ids = setting.AppId,
+                        data = new
+                        {
+                            title = dto.Title,
+                            content = dto.Content,
+                            bigContent = dto.BigContent,
+                            action = new { action_type = dto.ActionType, url = dto.Url }
+                        },
+                        is_draft = false,
+                        filter = new { custom_id = dto.UserIds }
+                    };
+
+                    CustomHttpClient<object, object> client = new();
+                    client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+                    client.DefaultRequestHeaders.Add("Authorization", "Token " + setting.Token);
+                    client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "Token " + setting.Token);
+
+                    object result = await client.Post("https://api.pushe.co/v2/messaging/notifications/", body);
+                    break;
+
+                    //RestRequest request = new(Method.POST);
+                    //request.AddHeader("Content-Type", "application/json");
+                    //request.AddHeader("Authorization", "Token " + setting.Token);
+                    //var body = new {
+                    //	app_ids = setting.AppId,
+                    //	data = new {
+                    //		title = dto.Title,
+                    //		content = dto.Content,
+                    //		bigContent = dto.BigContent,
+                    //		action = new {action_type = dto.ActionType, url = dto.Url}
+                    //	},
+                    //	is_draft = false,
+                    //	filter = new {custom_id = dto.UserIds}
+                    //};
+                    //request.AddJsonBody(body);
+
+                    //IRestResponse i = await new RestClient("https://api.pushe.co/v2/messaging/notifications/").ExecuteAsync(request);
+                }
+        }
+        return new GenericResponse();
+    }
 }
 
-public class NotificationCreateDto {
-	public IEnumerable<string> UserIds { get; set; } = new List<string>();
-	public string Title { get; set; } = "";
-	public string Content { get; set; } = "";
-	public string BigContent { get; set; } = "";
-	public string Url { get; set; } = "";
-	public string ActionType { get; set; } = "U";
+public class NotificationCreateDto
+{
+    public IEnumerable<string> UserIds { get; set; } = new List<string>();
+    public string Title { get; set; } = "";
+    public string Content { get; set; } = "";
+    public string BigContent { get; set; } = "";
+    public string Url { get; set; } = "";
+    public string ActionType { get; set; } = "U";
 }
