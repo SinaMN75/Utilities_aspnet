9dc1aa189da409ef9118b5c98c4d730bd72b2335
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-09-08 17:51:06 +0330

    :)

--- Controllers/ScrapperController.cs
+++ Controllers/ScrapperController.cs
@@ -9,6 +9,7 @@ public class ScrapperController : ControllerBase {
 	[HttpGet("GetInstaPostRapidApi/{username}")]
 	public string GetInstaPostRapidApi(string username) {
 		RestClient client = new($"https://193.36.85.225/api/Scrapper/GetInstaPostRapidApi/{username}");
+		client.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
 		RestRequest request = new(Method.GET);
 		IRestResponse response = client.Execute(request);
 		Console.WriteLine(response.Content);
