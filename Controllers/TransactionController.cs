6f5838256684b5048fd0226e6b83865c730dd334
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-09-23 17:05:01 +0330

    :)

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -8,13 +8,9 @@ public class TransactionController : BaseApiController {
 
 	public TransactionController(ITransactionRepository repository) => _repository = repository;
 
-	[HttpPost]
-	public async Task<ActionResult<GenericResponse<TransactionEntity>>> Create(TransactionEntity dto, CancellationToken ct) =>
-		Result(await _repository.Create(dto, ct));
-
-	[HttpGet]
+	[HttpPost("Filter")]
 	[OutputCache(PolicyName = "24h")]
-	public ActionResult<GenericResponse<IQueryable<TransactionEntity>>> Read(TransactionFilterDto dto) => Result(_repository.Filter(dto));
+	public ActionResult<GenericResponse<IQueryable<TransactionEntity>>> Filter(TransactionFilterDto dto) => Result(_repository.Filter(dto));
 
 	[HttpGet("Mine")]
 	public ActionResult<GenericResponse<IQueryable<TransactionEntity>>> ReadMine() => Result(_repository.ReadMine());
