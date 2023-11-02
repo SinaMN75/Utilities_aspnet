b820f8ba84a32144a405273f1a55105704e4e961
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-11-02 18:24:47 +0330

    :)

--- Controllers/Content2Controller.cs
+++ Controllers/Content2Controller.cs
@@ -2,22 +2,22 @@ namespace Utilities_aspnet.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class Content2Controller(IContentRepository repository) : BaseApiController {
+public class Content2Controller(IContentRepository2 repository) : BaseApiController {
 	[HttpGet]
 	[OutputCache(PolicyName = "24h")]
 	public ActionResult<GenericResponse<IQueryable<ContentEntity>>> Read() => Result(repository.Read());
 
-	[HttpPost]
-	[Authorize]
-	public async Task<ActionResult<GenericResponse<ContentEntity>>> Create(ContentCreateUpdateDto dto, CancellationToken ct) =>
-		Result(await repository.Create(dto, ct));
-
-	[HttpPut]
-	[Authorize]
-	public async Task<ActionResult<GenericResponse<ContentEntity>>> Update(ContentCreateUpdateDto dto, CancellationToken ct) =>
-		Result(await repository.Update(dto, ct));
-
-	[HttpDelete("{id:guid}")]
-	[Authorize]
-	public async Task<IActionResult> Delete(Guid id, CancellationToken ct) => Result(await repository.Delete(id, ct));
+	// [HttpPost]
+	// [Authorize]
+	// public async Task<ActionResult<GenericResponse<ContentEntity>>> Create(ContentCreateUpdateDto dto, CancellationToken ct) =>
+	// 	Result(await repository.Create(dto, ct));
+	//
+	// [HttpPut]
+	// [Authorize]
+	// public async Task<ActionResult<GenericResponse<ContentEntity>>> Update(ContentCreateUpdateDto dto, CancellationToken ct) =>
+	// 	Result(await repository.Update(dto, ct));
+	//
+	// [HttpDelete("{id:guid}")]
+	// [Authorize]
+	// public async Task<IActionResult> Delete(Guid id, CancellationToken ct) => Result(await repository.Delete(id, ct));
 }
