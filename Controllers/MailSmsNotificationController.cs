647c0f42ffaffd188d06067c8486213a8aa3d3ad
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-12-09 10:58:25 +0330

    :)

--- Controllers/MailSmsNotificationController.cs
+++ Controllers/MailSmsNotificationController.cs
@@ -7,11 +7,14 @@ public class MailSmsNotificationController(ISmsNotificationRepository repository
 	public async Task<ActionResult<GenericResponse>> SendNotification(NotificationCreateDto dto) => Result(await repository.SendNotification(dto));
 
 	[HttpPost("SendOtpSms")]
-	public async Task<ActionResult<GenericResponse>> SendOtpSms(string mobileNumber,
-		string param1,
-		string? template,
-		string? param2 = null,
-		string? param3 = null
-	) =>
-		Result(await repository.SendSms(mobileNumber, template, param1, param2, param3));
+	public async Task<ActionResult<GenericResponse>> SendOtpSms(SendOtpSmsDto dto) =>
+		Result(await repository.SendSms(dto.MobileNumber, dto.Param1, dto.Template, dto.Param2, dto.Param3));
+}
+
+public class SendOtpSmsDto {
+	public required string MobileNumber { get; set; }
+	public required string Param1 { get; set; }
+	public required string Template { get; set; }
+	public string? Param2 { get; set; }
+	public string? Param3 { get; set; }
 }
