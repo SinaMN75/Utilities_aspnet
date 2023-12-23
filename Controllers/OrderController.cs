153bbc8e913ad270a2dfe3075e19d07e73b91399
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-12-23 10:11:58 +0330

    :)

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -2,9 +2,9 @@ namespace Utilities_aspnet.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-[Authorize]
 public class OrderController(IOrderRepository repository) : BaseApiController {
 	[HttpPut]
+	[Authorize]
 	public async Task<ActionResult<GenericResponse<OrderEntity?>>> Update(OrderCreateUpdateDto dto) => Result(await repository.Update(dto));
 
 	[HttpPost("Filter")]
@@ -14,20 +14,25 @@ public class OrderController(IOrderRepository repository) : BaseApiController {
 	public async Task<ActionResult<GenericResponse<OrderEntity>>> ReadById(Guid id) => Result(await repository.ReadById(id));
 
 	[HttpPost("Vote")]
+	[Authorize]
 	public async Task<ActionResult<GenericResponse>> Vote(OrderVoteDto dto) => Result(await repository.Vote(dto));
 
 	[HttpDelete("{id}")]
+	[Authorize]
 	public async Task<ActionResult<GenericResponse<OrderEntity>>> Delete(Guid id) => Result(await repository.Delete(id));
 
 	[HttpPost("CreateUpdateOrderDetail")]
+	[Authorize]
 	public async Task<ActionResult<GenericResponse<OrderEntity>>> CreateUpdateOrderDetail(OrderDetailCreateUpdateDto dto) =>
 		Result(await repository.CreateUpdateOrderDetail(dto));
 	
 	[HttpPost("CreateReservationOrder")]
+	[Authorize]
 	public async Task<ActionResult<GenericResponse<OrderEntity>>> CreateReservationOrder(ReserveCreateUpdateDto dto) =>
 		Result(await repository.CreateReservationOrder(dto));		
 	
 	[HttpPost("CreateChairReservationOrder")]
+	[Authorize]
 	public async Task<ActionResult<GenericResponse<OrderEntity>>> CreateChairReservationOrder(ReserveChairCreateUpdateDto dto) =>
 		Result(await repository.CreateChairReservationOrder(dto));	
 }
