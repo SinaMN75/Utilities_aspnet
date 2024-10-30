a10e8091e44e779ee2bc0393339eee131c9d0223
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-10-30 11:07:42 +0330

    :)

--- Controllers/ProductV2Controller.cs
+++ Controllers/ProductV2Controller.cs
@@ -5,37 +5,46 @@ namespace Utilities_aspnet.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 [ApiKey]
-public class ProductV2Controller(IProductRepository repository) : BaseApiController {
+public class ProductV2Controller(IProductRepository repository, IOutputCacheStore store) : BaseApiController {
 	[Authorize]
 	[HttpPost]
-	public async Task<ActionResult<GenericResponse<ProductEntity>>> Create(ProductCreateUpdateDto dto, CancellationToken ct) =>
-		Result(await repository.Create(dto, ct));
+	public async Task<ActionResult<GenericResponse<ProductEntity>>> Create(ProductCreateUpdateDto dto, CancellationToken ct) {
+		await store.EvictByTagAsync("product", ct);
+		return Result(await repository.Create(dto, ct));
+	}
 
 	[Authorize]
 	[AllowAnonymous]
-	[OutputCache(Duration = 1)]
+	[OutputCache(PolicyName = "default", Tags = ["product"])]
 	[HttpPost("Filter")]
-	public async Task<ActionResult<GenericResponse<IQueryable<ProductEntity>>>> Filter(ProductFilterDto dto) =>
-		Result(await repository.Filter(dto));
+	public async Task<ActionResult<GenericResponse<IQueryable<ProductEntity>>>> Filter(ProductFilterDto dto) {
+		return Result(await repository.Filter(dto));
+	}
 
 	[Authorize]
 	[AllowAnonymous]
 	[HttpGet("{id:guid}")]
-	public async Task<ActionResult<GenericResponse<ProductEntity>>> ReadById(Guid id, CancellationToken ct) =>
+	public async Task<ActionResult<GenericResponse<ProductEntity>>> ReadById(Guid id, CancellationToken ct) => 
 		Result(await repository.ReadById(id, ct));
 
 	[HttpPut]
 	[Authorize]
-	public async Task<ActionResult<GenericResponse<ProductEntity>>> Update(ProductCreateUpdateDto dto, CancellationToken ct) =>
-		Result(await repository.Update(dto, ct));
+	public async Task<ActionResult<GenericResponse<ProductEntity>>> Update(ProductCreateUpdateDto dto, CancellationToken ct) {
+		await store.EvictByTagAsync("product", ct);
+		return Result(await repository.Update(dto, ct));
+	}
 
 	[Authorize]
 	[HttpDelete("{id:guid}")]
-	public async Task<IActionResult> Delete(Guid id, CancellationToken ct) =>
-		Result(await repository.Delete(id, ct));
+	public async Task<IActionResult> Delete(Guid id, CancellationToken ct) {
+		await store.EvictByTagAsync("product", ct);
+		return Result(await repository.Delete(id, ct));
+	}
 
 	[Authorize]
 	[HttpPost("CreateReaction")]
-	public async Task<ActionResult<GenericResponse>> CreateReaction(ReactionCreateUpdateDto dto) =>
-		Result(await repository.CreateReaction(dto));
+	public async Task<ActionResult<GenericResponse>> CreateReaction(ReactionCreateUpdateDto dto, CancellationToken ct) {
+		await store.EvictByTagAsync("product", ct);
+		return Result(await repository.CreateReaction(dto));
+	}
 }
