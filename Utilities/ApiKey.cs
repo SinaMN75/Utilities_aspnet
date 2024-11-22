c52489ef72f87d5e5731307709f89955bacc48cc
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-11-22 20:51:14 +0330

    :)

--- Utilities/ApiKey.cs
+++ Utilities/ApiKey.cs
@@ -3,21 +3,6 @@ namespace Utilities_aspnet.Utilities;
 //
 // Api Response Encryption
 //
-
-public class ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration) {
-	public async Task Invoke(HttpContext context) {
-		if ((context.Request.Path.Value ?? "").Contains("api"))
-			if (!context.Request.Headers.TryGetValue("X-API-Key", out StringValues apiKey) || apiKey != configuration.GetValue<string>("ApiKey")!) {
-				context.Response.StatusCode = StatusCodes.Status404NotFound;
-				return;
-			}
-
-		await next(context);
-	}
-}
-//
-// Api Response Encryption
-//
 public class EncryptResponseAttribute : ActionFilterAttribute {
 	public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next) {
 		if (context.Result is ObjectResult { Value: not null } objectResult) {
